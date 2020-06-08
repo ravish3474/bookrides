587aaed28c604974d55264a51d7848158b606571
MIRZA RAVISH <ravish3474@outlook.com>
2020-06-09 02:59:37 +0530

    rav

--- core/server.js
+++ core/server.js
@@ -9,9 +9,11 @@ const supplierRoutes = require('../api/routes/suppliers');
 const vehicleRoutes = require('../api/routes/vehicles');
 const locationRoutes = require('../api/routes/location');
 const inventoryRoutes = require('../api/routes/inventory');
-var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.json({limit: "50mb"}));
+// app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 var fs = require('fs');
 var path = require('path');
